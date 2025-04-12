e38f17917b6d095297f175ecf9f0cff19f5fde7e
Sudheer284 <sudheerreddy.gone@gmail.com>
2025-04-12 18:38:56 +0530

    Add files via upload

---
+++ convertion.py
@@ -0,0 +1,19 @@
+#float --> int
+a=69.5
+b=int(a)
+print(b)
+
+#string --> int
+c='56'
+d=int(c)
+print(d)
+
+#string --> float
+e='12.5'
+f=float(e)
+print(f)
+
+#int --> string
+g=18
+h=str(g)
+print(h)
